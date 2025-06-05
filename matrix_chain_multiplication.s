e5bb9f82135602f54fe73e93388281df4cdc12fa
MJThinker <pihang.tusung@gmail.com>
2025-06-05 19:57:22 +0800

    Initialization

---
+++ matrix_chain_multiplication.s
@@ -0,0 +1,12 @@
+.text
+.globl matrix_chain_multiplication
+
+matrix_chain_multiplication:
+
+    // Your implementation here
+
+
+
+    // Return to main program after completion (Remember to store the return address at the beginning)
+    // jr ra
+
